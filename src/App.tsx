40664efd82b04f2671646a883c743981e5404171
Piyush Joshi <piyush.joshi01@nagarro.com>
2023-04-17 22:08:23 +0530

    assignment 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,100 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [item,setItem]=useState('');
+
+  const [quantity,setQuantity]=useState('');
+  const [list,addList]=useState([] as any);
+  
+  const addItem = (event:any)=>{
+    event.preventDefault();
+    let data =
+      {
+        id:(Math.random()*1000).toFixed(0),
+        itemName : item,
+        itemQuantity : quantity,
+      }
+    
+    let  templist=list
+    templist.push(data)
+    addList(templist)
+    console.log(list)
+    setItem('');
+    setQuantity('');
+  };
+  const add =(id:any)=>{
+    let  newlist =list.filter((items:any)=>items.id==id)
+    // newlist[0].itemQuantity=(newlist[0].itemQquantity+1);
+    newlist[0].itemQuantity=parseInt(newlist[0].itemQuantity)+1;
+    let templist=list.filter((items:any)=>items.id!==id)
+    templist.push(newlist[0]); 
+    // list.push(newlist);
+    addList(templist);
+    
+    console.log(templist)
+    setItem('');
+    setQuantity('');
+  }
+  const subtract=(id:any)=>{
+    let  newlist =list.filter((items:any)=>items.id==id)
+    // newlist[0].itemQuantity=(newlist[0].itemQquantity+1);
+    newlist[0].itemQuantity=parseInt(newlist[0].itemQuantity)-1;
+    let templist=list.filter((items:any)=>items.id!==id)
+    templist.push(newlist[0]); 
+    // list.push(newlist);
+    addList(templist);
+    
+    console.log(templist)
+    
+  }
+  const remove = (id:any)=>{
+    let newList = list.filter((item: any) => item.id !== id);
+    addList(newList);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='store'>
+        <section>
+          <h1>store</h1>
+        </section>
+    <form onSubmit={addItem}>
+      <label htmlFor="item">Item</label>
+    <br />
+    <input type="text" id="item" name='item' onChange={(e)=>setItem(e.target.value)} value={item} />
+    <br />
+    <label htmlFor="quantity">quantity</label>
+    <br />
+    <input type="number" id="quantity" name='quantity'onChange={(e)=>setQuantity(e.target.value) } value={quantity} />
+    <br />
+    <button type='submit' >add</button>
+    
+    </form>
+      </section>
+      <section className='showData'>
+        <h1>cart</h1>
+
+      </section>
+      <section>
+      
+         {
+         list.map((items:any)=>(
+          
+          <div key={items.id} >{items.itemName} X {items.itemQuantity} <br />
+          <button onClick={()=>add(items.id)}>+</button> 
+          <button onClick={()=>subtract(items.id)}>-</button> 
+          <button onClick={()=>remove(items.id)}>delete</button> </div>
+          
+        ))
+      }
+       
+
+
+       
+      </section>
     </div>
   );
 }
